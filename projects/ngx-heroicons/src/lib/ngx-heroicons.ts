1c90a424147ee2fd41ab041dc0215f9ff1b03ace
pm3cx <pm@3cx.com>
2026-06-05 11:19:10 +0300

    initial library

---
+++ projects/ngx-heroicons/src/lib/ngx-heroicons.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-heroicons',
+  imports: [],
+  template: ` <p>ngx-heroicons works!</p> `,
+  styles: ``,
+})
+export class NgxHeroicons {}
