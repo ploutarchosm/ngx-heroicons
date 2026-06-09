82f7adf77acbc1fd4451c4334238c0af437acd4c
pm3cx <pm@3cx.com>
2026-06-09 11:42:19 +0300

    update

--- projects/ngx-heroicons/src/public-api.ts
+++ projects/ngx-heroicons/src/public-api.ts
@@ -6,6 +6,3 @@
 export * from './lib/ngx-heroicons-base';
 export * from './lib/ngx-heroicons-provide';
 export * from './lib/ngx-heroicons';
-
-// All generated icon components (tree-shaken: only what you import is bundled)
-export * from './lib/icons';
